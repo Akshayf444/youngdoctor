57ed2b66ee0164ee733099aa47057889c3468387
Akshayf444 <mohite.akshay118@gmail.com>
2016-05-21 11:44:35 +0530

    Added Responsive Table Layout

--- application/views/User/view_pgdoctor.php
+++ application/views/User/view_pgdoctor.php
@@ -14,7 +14,7 @@
 
     </div>
 </div>
-<a download="Doctor<?php // echo date('dM g-i-a');          ?>.xls" class="btn btn-success  " href="#" onclick="return ExcellentExport.excel(this, 'datatable', 'Sheeting');"><i class="fa fa-arrow-circle-o-right"></i> Export to Excel </a>
+<a download="Doctor<?php // echo date('dM g-i-a');            ?>.xls" class="btn btn-success  " href="#" onclick="return ExcellentExport.excel(this, 'datatable', 'Sheeting');"><i class="fa fa-arrow-circle-o-right"></i> Export to Excel </a>
 <div class="row">
     <div class="col-lg-12">
         <table class="table table-bordered table-hover panel" id="datatable">
@@ -45,30 +45,26 @@
                 if (!empty($show)) {
                     foreach ($show as $row) :
                         ?><tr>  
-                            <td><?php echo $count++; ?></td>
-                            <td><?php echo $row->State; ?></td>
-                            <td><?php echo $row->Region; ?></td>
-                            <td><?php echo $row->Doctor_Name; ?></td>  
-                            <td><?php echo $row->MSL_Code; ?> </td> 
-                            <td><?php echo $row->address; ?></td>  
-                            <td><?php echo $row->Institution; ?></td>
-                            <td><?php echo $row->Mobile_Number; ?></td>
-                            <td><?php echo $row->email; ?></td>  
-                            <td><?php echo $row->Years_Practice; ?></td>
-                            <td><?php echo $row->DOB; ?></td>   
-                            <td><?php echo $row->ANNIVERSARY; ?></td>
-                            <td><?php echo $row->CiplaSerice; ?></td>
-                            <td><?php echo $row->FITB; ?></td>
-                            <td><?php echo $row->BM_Name; ?></td>
-                            <td><?php echo $row->SM_Name; ?></td>
-
-
-                            <td>  
+                            <td data-title="Sr"><?php echo $count++; ?></td>
+                            <td data-title="State"><?php echo $row->State; ?></td>
+                            <td data-title="Region"><?php echo $row->Region; ?></td>
+                            <td data-title="Doctor Name"><?php echo $row->Doctor_Name; ?></td>  
+                            <td data-title="MSL Code"><?php echo $row->MSL_Code; ?> </td> 
+                            <td data-title="Address"><?php echo $row->address; ?></td>  
+                            <td data-title="Institution"><?php echo $row->Institution; ?></td>
+                            <td data-title="Mobile Number"><?php echo $row->Mobile_Number; ?></td>
+                            <td data-title="Email"><?php echo $row->email; ?></td>  
+                            <td data-title="Years Of Practice"><?php echo $row->Years_Practice; ?></td>
+                            <td data-title="DOB"><?php echo $row->DOB; ?></td>   
+                            <td data-title="Clinic Anniversary"><?php echo $row->ANNIVERSARY; ?></td>
+                            <td data-title="Name Of Clipa Services"><?php echo $row->CiplaSerice; ?></td>
+                            <td data-title="FITB Done"><?php echo $row->FITB; ?></td>
+                            <td data-title="BM"><?php echo $row->BM_Name; ?></td>
+                            <td data-title="SM"><?php echo $row->SM_Name; ?></td>
+                            <td data-title="Action">  
                                 <a class="fa fa-trash-o btn-danger btn-xs" class=""  onclick="deletedoc('<?php echo site_url('User/pgdoc_del?id=') . $row->DoctorId; ?>')"></a> 
                               <!--<a class="fa fa-pencil btn-success btn-xs" onclick="window.location = '<?php echo site_url('User/pgdoc_update?id=') . $row->DoctorId; ?>';"></a>-->                                 
-
                             </td>
-
                         </tr>
                         <?php
                     endforeach;
